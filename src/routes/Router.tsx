5ea5686abd955baacb76c0b237962e239c9e2a75
santi lubary <santiago.lp.cop@gmail.com>
2021-06-06 14:45:56 -0300

    sacadas rutas login signup

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../components/Layout'
 
 import Main from '../components/Main'
-import Signup from '../components/Signup'
-import Login from '../components/Login'
 
 const Router = () => {
     return (
@@ -11,8 +9,6 @@ const Router = () => {
             <Layout>
                 <Switch>
                     <Route exact path="/" component={Main} />
-                    <Route exact path="/signup" component={Signup} />
-                    <Route exact path="/login" component={Login} />
                     <Route component={Main} />
                 </Switch>
             </Layout>

90f60f0563555dcf8f2fc82437a96ceec8642d63
SaniLubary <santiago.lp.cop@gmail.com>
2021-07-12 19:33:34 -0300

    basic quote crud protected with user authencitation & modified kernel.php for the api to use bearer token, and not session-cookie auth

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,16 +1,26 @@
 <?php
 
 use App\http\Controllers\QuoteController;
+use App\http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/// Public Routes
 
+// Register
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Quotes
 Route::get('/quotes', [QuoteController::class, 'index']);
 Route::get('/quotes/{id}', [QuoteController::class, 'show']);
-Route::post('/quotes', [QuoteController::class, 'store']);
-Route::put('/quotes/{id}', [QuoteController::class, 'update']);
-Route::delete('/quotes/{id}', [QuoteController::class, 'destroy']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/// Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Quotes routes
+    Route::post('/quotes', [QuoteController::class, 'store']);
+    Route::put('/quotes/{id}', [QuoteController::class, 'update']);
+    Route::delete('/quotes/{id}', [QuoteController::class, 'destroy']);    
 });
